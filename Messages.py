f5b401e338f229890e814bad15a4f3e1acab0f0f
Erdélyi <erdelyi.adam95@gmail.com>
2019-05-06 19:05:53 +0200

    Basic initialisation message creation with create_INI_message()

--- Messages.py
+++ Messages.py
@@ -6,8 +6,7 @@ import time
 
 def create_DAT_message(msg, key, sign_key, sender):
     """
-    Create DAT type message. This type of msg
-
+    Create DAT type message. This type of message is for transmitting the chat messages.
 
     :param msg: string - Message to send
     :param key: Key to CBC
@@ -25,5 +24,26 @@ def create_DAT_message(msg, key, sign_key, sender):
     data = msgtype + iv + ciphertext
     sign = Sign.generate_signature(data, sign_key)
     data = data + sign
+
+    return data
+
+
+def create_INI_message(pubkey, sign_key, sender):
+    """
+    Create INI type message. This type of message is for initialize the connection.
+
+    :param pubkey: The public key of the source
+    :param sign_key: Key to sign with
+    :param sender: string - Source of the message
+    :return: The framed message
+    """
+    msgtype = "INI".encode('utf-8')
+    timestamp = int(time.time()).to_bytes(5, 'big')
+    sender = sender.encode('utf-8')
+    pubkey = pubkey.exportKey(format='PEM')
+
+    plaintext = msgtype + timestamp + sender + pubkey
+    sign = Sign.generate_signature(plaintext, sign_key)
+    data = plaintext + sign
 
     return data
