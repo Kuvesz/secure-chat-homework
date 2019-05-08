116f04484cab654f519c809308edbc3b08e7382c
Erdélyi <erdelyi.adam95@gmail.com>
2019-05-08 03:17:01 +0200

    Working TCP server client connection

--- client.py
+++ client.py
@@ -1,8 +1,27 @@
-import requests 
+import requests
+import socket
+import os
 
-while True:
-    data = input('Please enter your message:')
-    url = "http://127.0.0.1:8080"
+def main():
+    print("Welcome to the most secure multi chat application!")
 
-    r = requests.post(url = url, data = data)
-    print(r.text)
+    name = ""
+    while len(name) != 5:
+        name = input("Please enter your name(exactly 5 character):")
+
+    ip = input("Please enter the IP address of the secure server:")
+    port = input("Please enter the port number of the secure server:")
+    url = "http://" + ip + ":" + port
+    print(url)
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ip, int(port)))
+
+    # response = os.system("ping -n 1 " + ip)
+    # print(response)
+
+    s.sendall(b'Hello, world')
+    data = s.recv(1024)
+    s.close()
+    print('Received', repr(data))
+
+main()
--- server.py
+++ server.py
@@ -1,26 +1,14 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
+import socket
 
-from io import BytesIO
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('localhost', 8080))
+s.listen(1)
+conn, addr = s.accept()
 
+while 1:
+    data = conn.recv(1024)
+    if not data:
+        break
+    conn.sendall(data)
 
-class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Hello, world!')
-
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
-        body = self.rfile.read(content_length)
-        self.send_response(200)
-        self.end_headers()
-        response = BytesIO()
-        response.write(b'This is POST request. ')
-        response.write(b'Received: ')
-        response.write(body)
-        self.wfile.write(response.getvalue())
-
-
-httpd = HTTPServer(('localhost', 8080), SimpleHTTPRequestHandler)
-httpd.serve_forever()
+conn.close()
