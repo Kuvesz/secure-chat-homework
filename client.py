804cc0c4faf756ac8a38b95b27fde90b54d6e24a
Erdélyi <erdelyi.adam95@gmail.com>
2019-05-08 14:26:54 +0200

    Starting INIT phase

--- client.py
+++ client.py
@@ -1,27 +1,102 @@
 import requests
 import socket
+import Messages
+import RSA_Helper as RSA
+import AES_Helper as AES
+import Crypto.PublicKey.RSA
 import os
+
+sign_keys =	{
+    "Alice": "Alice_public_sign_key.pem",
+    "Bob__": "Bob___public_sign_key.pem",
+    "Carol": "Carol_public_sign_key.pem",
+    "dummy": "dummy_public_sign_key.pem",
+    "Serve": "Server_public_sign_key.pem"
+} ## Need to be exported to a file
 
 def main():
     print("Welcome to the most secure multi chat application!")
 
     name = ""
     while len(name) != 5:
-        name = input("Please enter your name(exactly 5 character):")
+        name = input("Please enter your name(exactly 5 character): ")
 
-    ip = input("Please enter the IP address of the secure server:")
-    port = input("Please enter the port number of the secure server:")
-    url = "http://" + ip + ":" + port
-    print(url)
+    # ip = input("Please enter the IP address of the secure server: ")
+    # port = input("Please enter the port number of the secure server: ")
+    ip = "localhost"
+    port = "8080"
+
+    sender = name
+    # import own keys
+    try:
+        rsa_key = RSA.import_key(sender + "_rsa_key.pem")
+        sign_key = RSA.import_key(sender + "_sign_key.pem")
+    except: #only for debug
+        rsa_key = RSA.import_key("keypair.pem")
+        sign_key = RSA.import_key("keypair.pem")
+
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ip, int(port)))
 
-    # response = os.system("ping -n 1 " + ip)
-    # print(response)
+    print("Connected to server\n")
 
-    s.sendall(b'Hello, world')
-    data = s.recv(1024)
+    #initialize the Connection
+    init = Messages.create_INI_message(rsa_key.publickey(), sign_key, sender)
+    s.sendall(init)
+    s.settimeout(5)
+    while True:
+        try:
+            data = s.recv(4096)
+            mes = Messages.Message()
+            mes.process_message(data, None, sign_keys)
+            if mes.msgtype == "INI" and mes.sender == "Serve":
+                aes_key = AES.generate_key()
+                print("DONE")
+                break
+            if mes.msgtype == "KEY":
+                print("KEYWAIT")
+                aes_key = mes.msg.encode('utf-8')
+                break
+        except:
+            continue
+        # elif mes.msgtype == "INI" and mes.validity == True:
+    #     print("ELSE")
+    #     pass
+    s.settimeout(2)
+
+    while True:
+
+        try:
+            print("wait")
+            data = s.recv(4096)
+            print("stsh")
+            mes = Messages.Message()
+            mes.process_message(data, aes_key, sign_keys)
+            print(mes.validity)
+            print(mes.sender)
+            print(mes.msgtype)
+            if mes.msgtype == "INI":
+                print("in")
+                data = Messages.create_KEY_message(aes_key, Crypto.PublicKey.RSA.import_key(mes.msg.decode('utf-8')), sign_key, sender)
+                s.sendall(data)
+                print("KEYSENT")
+
+
+        except:
+            continue
+
+        print('Received', repr(data))
+
+        msg = input("input: ")
+
+        if msg == "END_CONN":
+            print("Connection is closed.")
+            break
+
+        s.sendall(msg.encode('utf-8'))
+
+
+    s.shutdown(socket.SHUT_RDWR)
     s.close()
-    print('Received', repr(data))
 
 main()
--- server.py
+++ server.py
@@ -1,14 +1,80 @@
 import socket
+import Messages
+import RSA_Helper as RSA
+
+
+sign_keys =	{
+    "Alice": "Alice_public_sign_key.pem",
+    "Bob__": "Bob___public_sign_key.pem",
+    "Carol": "Carol_public_sign_key.pem",
+    "dummy": "dummy_public_sign_key.pem",
+    "Serve": "Server_public_sign_key.pem"
+}
+
+sender = "Serve"
+sign_key = RSA.import_key("Server_sign_key.pem")
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# s.setblocking(False)
+s.settimeout(1)
 s.bind(('localhost', 8080))
 s.listen(1)
-conn, addr = s.accept()
+# conn, addr = s.accept()
+print("1")
+
+conn = []
 
 while 1:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.sendall(data)
+    print("2")
+    try:
+        c, addr = s.accept()
+        c.settimeout(1)
+        conn.append(c)
+    except:
+        if len(conn) == 0:
+            continue
+        else:
+            print("len", len(conn))
+            pass
+    print("3")
 
-conn.close()
+    for i in range(0, len(conn)):
+        print("for", i)
+        try:
+            data = conn[i].recv(4096)
+
+            # Close connection
+            if not data:
+                conn[i].shutdown(socket.SHUT_RDWR)
+                conn[i].close()
+                del conn[i]
+                break
+
+            mes = Messages.Message()
+            mes.process_message(data, None, sign_keys)
+            print(mes.sender)
+            print(mes.validity)
+            print(mes.msgtype)
+
+            # Process INI messages
+            if mes.msgtype == "INI":
+                print("init")
+                if len(conn) == 1:
+                    data = Messages.create_INI_message(sign_key.publickey(), sign_key, sender)
+                    conn[i].sendall(data)
+                else:
+                    conn[0].sendall(data)
+                    print("else")
+            elif mes.msgtype == "KEY":
+                print("key")
+                conn[len(conn)-1].sendall(data)
+            else:
+                conn[i].sendall(data)
+        except:
+            continue
+
+    print("4")
+
+
+# conn.shutdown(socket.SHUT_RDWR)
+# conn.close()
